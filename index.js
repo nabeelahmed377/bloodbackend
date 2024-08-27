505fbebb4ca6be6657214df669f1320734d0a4d8
nabeel ahmed <nabeelahmed377@gmail.com>
2024-08-27 14:28:00 +0500

    COMMIit

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const corsOptions = {
 // add middlewares
 app.use(express.json());
 app.use(cors(corsOptions));
+app.options("", cors(corsOptions));
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/patient", userRoute);
